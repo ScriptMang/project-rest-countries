cf38b4c9ab6d0a29d49d74e17c7b223824056e4f
scriptMang <aperalta686@gmail.com>
2026-02-10 00:36:22 -0500

    Use the fetch api to get all needed fields for the country flag cards

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ console.log("Script: hello-world")
 // flags,name,population,region, capital
 fetch('https://restcountries.com/v3.1/all?fields=flags,name,population,region,capital')
 .then(resp => resp.json())
-.then(data => console.log("Fetched data:", data))
+.then(data => console.log(data[0].name))       
 .catch(error => console.error("Fetch error:", error));
