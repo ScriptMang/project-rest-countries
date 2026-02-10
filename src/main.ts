e3170ccd0d63165f4c4b3fcc35a488fbee82ccb3
scriptMang <aperalta686@gmail.com>
2026-02-09 20:48:28 -0500

    Add fetch request for all names in countries-rest-api

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
 console.log("Script: hello-world")
+fetch('https://restcountries.com/v3.1/all?fields=name')
+.then(resp => resp.json())
+.then(data => console.log("Fetched data:", data))
+.catch(error => console.error("Fetch error:", error));
