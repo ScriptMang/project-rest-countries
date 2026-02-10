10d9f2322eed034477f3fee0a2f87676f2482697
scriptMang <aperalta686@gmail.com>
2026-02-09 21:24:36 -0500

    Add fetch request for all fields needed for a country-card

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 console.log("Script: hello-world")
-fetch('https://restcountries.com/v3.1/all?fields=name')
+// flags,name,population,region, capital
+fetch('https://restcountries.com/v3.1/all?fields=flags,name,population,region,capital')
 .then(resp => resp.json())
 .then(data => console.log("Fetched data:", data))
 .catch(error => console.error("Fetch error:", error));
