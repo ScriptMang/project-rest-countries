f49331a6e532a047fb6a047fc4dad846595e1c38
scriptMang <aperalta686@gmail.com>
2026-02-11 00:01:53 -0500

    Add country-ts file with country class to hold country info

---
+++ src/models/Country.ts
@@ -0,0 +1,35 @@
+class Country {
+    flagUrl: string
+    commonName: string
+    nativeName: string
+    population: number
+    region: string
+    subRegion: string
+    capital: string
+    topLevelDomain: string
+    currencies: string[]
+    languages: string[]
+
+
+    constructor(
+        flagUrl: string, commonName: string, nativeName: string,
+        population: number, region: string, subRegion: string,
+        capital: string, topLevelDomain: string, currencies: string[],
+        languages: string[]
+    ) {
+        this.flagUrl = flagUrl;
+        this.commonName =  commonName;
+        this.nativeName =  nativeName;
+        this.population = population;
+        this.region = region;
+        this.subRegion = subRegion;
+        this.capital = capital;
+        this.topLevelDomain = topLevelDomain;
+        this.currencies = currencies;
+        this.languages = languages;
+    }
+
+
+
+
+}
